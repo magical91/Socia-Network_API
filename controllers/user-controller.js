e4330cbd7975bf885cabb37d7748e16d7a8c12c4
magicalbeauty91 <magicalbeauty91@gmail.com>
2022-06-24 17:08:55 -0500

    fixed bug for user model and controller

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -3,10 +3,10 @@ const { User, Thought } = require('../models/index');
 const userController = {
     getAllUsers(req, res) {
         User.find({})
-            .populate({
-                path: 'thoughts',
-                select: '-__v'
-            })
+            // .populate({
+            //     path: 'thoughts',
+            //     select: '-__v'
+            // })
             .select('-__v')
             .then(dbUserData => res.json(dbUserData))
             .catch(err => {
@@ -18,14 +18,14 @@ const userController = {
 
     getUserById({ params }, res) {
         User.findOne({ _id: params.id })
-            .populate({
-                path: 'thoughts',
-                select: '-__v'
-            })
-            .populate({
-                path: 'friends',
-                select: '-__v'
-            })
+            // .populate({
+            //     path: 'thoughts',
+            //     select: '-__v'
+            // })
+            // .populate({
+            //     path: 'friends',
+            //     select: '-__v'
+            // })
             .select('-__v')
             .then(dbUserData => res.json(dbUserData))
             .catch(err => {
--- models/User.js
+++ models/User.js
@@ -16,23 +16,23 @@ const UserSchema = new Schema({
             /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/,
         ]
     },
-    thoughts:
-        [{
-            type: Schema.Types.ObjectId,
-            ref: 'Thought'
-        }],
-    friends:
-        [{
-            type: Schema.Types.ObjectId,
-            ref: 'User'
-        }],
+    // thoughts:
+    //     [{
+    //         type: Schema.Types.ObjectId,
+    //         ref: 'Thought'
+    //     }],
+    // friends:
+    //     [{
+    //         type: Schema.Types.ObjectId,
+    //         ref: 'User'
+    //     }],
 },
-    opt
+    // opt
 );
 
 const User = model('User', UserSchema);
 UserSchema.virtual('friendCount').get(function () {
-    return this.friends.length;
+    // return this.friends.length;
 });
 
 module.exports = User;
